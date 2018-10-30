d27fa934754dbef091be07c4be916f0721d9dca8
Suppasek <59070168@it.kmitl.ac.th>
2018-10-30 19:10:14 +0700

    water record เสร็จแล้วเอาไปเทสให้หน่อย

--- app/src/main/java/com/kmitl/vpower/waterusagechecker/WaterRecordFragment.java
+++ app/src/main/java/com/kmitl/vpower/waterusagechecker/WaterRecordFragment.java
@@ -23,6 +23,7 @@ import android.widget.ProgressBar;
 import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
+
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
@@ -53,8 +54,15 @@ public class WaterRecordFragment extends Fragment {
 
     private FirebaseFirestore mdb = FirebaseFirestore.getInstance();
     private FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+    private FirebaseAuth userAuth = FirebaseAuth.getInstance();
     private CollectionReference colRef;
+
     private Dialog dialog;
+    private Dialog errorDialog;
+    private Dialog verifyDialog;
+    private Dialog logoutDialog;
+    private TextView errorText;
+
     private ArrayList<String> monthList = new ArrayList<>();
     private ArrayList<String> yearList = new ArrayList<>();
     private ArrayList<String> roomList = new ArrayList<>();
@@ -111,18 +119,6 @@ public class WaterRecordFragment extends Fragment {
 
         getRate();
 
-//        roomSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-//            @Override
-//            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-//                getPreviousRecord();
-//            }
-//
-//            @Override
-//            public void onNothingSelected(AdapterView<?> parent) {
-//
-//            }
-//        });
-
         initAddBtn();
 
     }
@@ -144,8 +140,8 @@ public class WaterRecordFragment extends Fragment {
                 TextView unitText = dialog.findViewById(R.id.record_dialog_unit);
 
                 roomText.setText(room);
-                monthText.setText(year+"/"+month);
-                unitText.setText(unitStr+" หน่วย");
+                monthText.setText(year + "/" + month);
+                unitText.setText(unitStr + " หน่วย");
 
                 dialog.show();
 
@@ -157,24 +153,15 @@ public class WaterRecordFragment extends Fragment {
         if (recordExist) {
             Log.wtf("WRF", "create new record");
             createRecord();
-        }
-        else {
+        } else {
             colRef = mdb.collection("rooms").document("house_no " + room).collection("water_usage");
             colRef.whereEqualTo("year", year).whereEqualTo("month", month).limit(1).get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
                 @Override
                 public void onComplete(@NonNull final Task<QuerySnapshot> task1) {
                     if (task1.isSuccessful() && !task1.getResult().isEmpty()) {
                         //Record already exist
-                        mdb.collection("rooms").document("house_no " + room).collection("water_usage").document(year + "_" + month).delete().addOnCompleteListener(new OnCompleteListener<Void>() {
-                            @Override
-                            public void onComplete(@NonNull Task<Void> task) {
-                                previousRecordNo = Integer.parseInt(task1.getResult().getDocuments().get(0).get("recordNo").toString());
-                                recordExist = true;
-                                Log.wtf("WRF", "record = " + recordNo + " delete previous record");
-
-                                getPreviousRecord();
-                            }
-                        });
+                        previousRecordNo = Integer.parseInt(task1.getResult().getDocuments().get(0).get("recordNo").toString());
+                        verifyDialog.show();
                     } else if (task1.isSuccessful()) {
                         createRecord();
                     }
@@ -190,10 +177,20 @@ public class WaterRecordFragment extends Fragment {
             public void onComplete(@NonNull Task<QuerySnapshot> task) {
                 if (task.isSuccessful()) {
                     if (!task.getResult().isEmpty()) {
-                        recordNo = Integer.parseInt(task.getResult().getDocuments().get(0).get("recordNo").toString()) + 1;
-                         previousUnit = Integer.parseInt(task.getResult().getDocuments().get(0).get("recordUnit").toString());
-                        pushRecord();
-                        Log.wtf("WRF", "record = " + recordNo + " new record");
+                        int previousMonth = Integer.parseInt(task.getResult().getDocuments().get(0).get("month").toString());
+                        int previousYear = Integer.parseInt(task.getResult().getDocuments().get(0).get("year").toString());
+                        int thisMonth = Integer.parseInt(month);
+                        int thisYear = Integer.parseInt(year);
+                        if ((thisMonth == previousMonth + 1 && thisYear == previousYear) || ((thisMonth == 1 && previousMonth == 12) && (thisYear == previousYear + 1))) {
+                            recordNo = Integer.parseInt(task.getResult().getDocuments().get(0).get("recordNo").toString()) + 1;
+                            previousUnit = Integer.parseInt(task.getResult().getDocuments().get(0).get("recordUnit").toString());
+                            pushRecord();
+                            Log.wtf("WRF", "record = " + recordNo + " new record");
+                        } else {
+                            errorText.setText("กรุณาเลือกเดือนและปีให้ถูกต้อง");
+                            errorDialog.show();
+                            setProgressBar(false);
+                        }
                     } else {
                         recordNo = 1;
                         previousUnit = 0;
@@ -229,7 +226,84 @@ public class WaterRecordFragment extends Fragment {
                 dialog.dismiss();
             }
         });
+
+        errorDialog = new Dialog(getActivity());
+        errorDialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        errorDialog.setContentView(R.layout.error_dialog);
+
+        Button acceptButton2 = errorDialog.findViewById(R.id.error_dialog_accept);
+        acceptButton2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                errorDialog.dismiss();
+            }
+        });
+
+        errorText = errorDialog.findViewById(R.id.error_dialog_text);
+
+        verifyDialog = new Dialog(getActivity());
+        verifyDialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        verifyDialog.setContentView(R.layout.verify_dialog);
+        verifyDialog.getWindow().setLayout(getResources().getDisplayMetrics().widthPixels, getResources().getDisplayMetrics().heightPixels /2);
+
+        Button verifyButton = verifyDialog.findViewById(R.id.verify_dialog_accept);
+        verifyButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mdb.collection("rooms").document("house_no " + room).collection("water_usage").document(year + "_" + month).delete().addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+
+                        recordExist = true;
+                        Log.wtf("WRF", "record = " + recordNo + " delete previous record");
+                        verifyDialog.dismiss();
+                        setProgressBar(false);
+                        getPreviousRecord();
+                    }
+                });
+
+            }
+        });
+
+        Button cancelButton2 = verifyDialog.findViewById(R.id.verify_dialog_cancel);
+        cancelButton2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                setProgressBar(false);
+                verifyDialog.dismiss();
+            }
+        });
+
+        logoutDialog = new Dialog(getActivity());
+        logoutDialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        logoutDialog.setContentView(R.layout.logout_dialog);
+        logoutDialog.getWindow().setLayout(getResources().getDisplayMetrics().widthPixels, getResources().getDisplayMetrics().heightPixels /2);
+
+
+        Button logoutButton = logoutDialog.findViewById(R.id.logout_dialog_accept);
+        logoutButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                userAuth.signOut();
+                logoutDialog.dismiss();
+                getActivity().getSupportFragmentManager()
+                        .beginTransaction()
+                        .replace(R.id.main_view, new LoginFragment())
+                        .addToBackStack(null)
+                        .commit();
+            }
+        });
+
+        Button cancelButton3 = logoutDialog.findViewById(R.id.logout_dialog_cancel);
+        cancelButton3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                logoutDialog.dismiss();
+            }
+        });
+
     }
+
 
     private void getRate() {
         setProgressBar(true);
@@ -273,26 +347,19 @@ public class WaterRecordFragment extends Fragment {
                     Toast.makeText(getActivity(), "Failure", Toast.LENGTH_SHORT).show();
                 }
             });
-        }
-        else {
+        } else {
             setProgressBar(false);
-            Toast.makeText(getActivity(), "ยูนิทเดือนปัจจุบันต้องมากกว่าหรือเท่ากับยูนิทเดือนที่แล้ว", Toast.LENGTH_SHORT).show();
+            errorText.setText("กรุณาใส่เลขมิเตอร์น้ำให้ถูกต้อง");
+            errorDialog.show();
         }
     }
 
     private void setLogoutBtn() {
-        final FirebaseAuth userAuth = FirebaseAuth.getInstance();
-
         logoutBtn = getView().findViewById(R.id.toolbar_logout);
         logoutBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                userAuth.signOut();
-                getActivity().getSupportFragmentManager()
-                        .beginTransaction()
-                        .replace(R.id.main_view, new LoginFragment())
-                        .addToBackStack(null)
-                        .commit();
+                logoutDialog.show();
             }
         });
     }
