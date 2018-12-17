325bbe41f26c00d2f1a0bce0aeb36937665d0289
Panudet Sukontasuchot <59070130@it.kmitl.ac.th>
2018-12-17 20:03:31 +0700

    Fix Download Link Dialog

--- app/src/main/java/com/kmitl/vpower/waterusagechecker/OverallFragment.java
+++ app/src/main/java/com/kmitl/vpower/waterusagechecker/OverallFragment.java
@@ -12,6 +12,11 @@ import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.AlertDialog;
+import android.text.Spannable;
+import android.text.SpannableString;
+import android.text.TextUtils;
+import android.text.method.LinkMovementMethod;
+import android.text.util.Linkify;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -211,9 +216,9 @@ public class OverallFragment extends Fragment {
         View view = inflater.inflate(R.layout.url_dialog, null);
         builder.setView(view);
 
-        final EditText waterRate = (EditText) view.findViewById(R.id.url_dialog_download_url);
+        final EditText urlText = (EditText) view.findViewById(R.id.url_dialog_download_url);
 
-        waterRate.setText(downloadURL);
+        urlText.setText(downloadURL);
 
         builder.setPositiveButton("Copy", new DialogInterface.OnClickListener() {
             @Override
